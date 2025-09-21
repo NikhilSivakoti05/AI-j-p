b9324d2a4d079ff86a3373b9f4b3f93e7a62bc70
Nikhil Sivakoti <2300030644@kluniversity.in>
2025-09-21 22:43:52 +0530

    Change base path for Vite configuration

--- todo-jenkins/vite.config.js
+++ todo-jenkins/vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js (dev only)
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/PromptAI",
   server: {
     proxy: {
       '/api': 'http://localhost:8080', // your Spring Boot backend
