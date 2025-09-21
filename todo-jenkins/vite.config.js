ac7215489f4b54e3eb59374bf542ace007f718bb
Nikhil Sivakoti <2300030644@kluniversity.in>
2025-09-21 22:42:34 +0530

    cx

--- todo-jenkins/vite.config.js
+++ todo-jenkins/vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+// vite.config.js (dev only)
 export default defineConfig({
   plugins: [react()],
-  base : "/PromptAI/"
+  base: "/",
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080', // your Spring Boot backend
+    },
+  },
 })
